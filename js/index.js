682f28733f533f55b2e1e99b85a9c1ba3108ed02
AlexWebArchitect <alex.web.architect@gmail.com>
2017-01-26 13:21:11 +0300

    some stuff have been done

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
-/*function randomQuote() {
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const latit = position.coords.latitude;
+    const longit = position.coords.longitude;
+    $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  });
+}
+
+function randomQuote() {
   $.ajax({
-      url: "http://api.forismatic.com/api/1.0/?",
+      url: "api.openweathermap.org/data/2.5/weather?lat=latit&lon=longit&APPID=93bbb527a6fdda2a5bd8906b20da0565",
       dataType: "jsonp",
-      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      /*data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
       success: function( response ) {
         $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
           response.quoteText + "<br/>&dash; " + response.quoteAuthor + "</p>");
         $("#tweet").attr("href", "http://twitter.com/home/?status=" + response.quoteText +
           ' - ' + response.quoteAuthor);
-      }
+      }*/
   });
 }
 
@@ -18,4 +26,4 @@ $(function() {
 
 $("#getMessage").click(function(){
   randomQuote();
-});*/
+});
