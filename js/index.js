6eea63b1989066d91e95eb9f6bf8b02a15874688
AlexWebArchitect <alex.web.architect@gmail.com>
2017-02-05 12:05:14 +0300

    background image change depending on the weather

--- js/index.js
+++ js/index.js
@@ -33,30 +33,39 @@ function mainFunction() {
             switch (weather.weather[0].description) {
                 case "clear sky":
                     $(".sunny").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/us3JQvC.jpg" + ')');
                     break;
                 case "few clouds":
                     $(".cloudy").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/VEkmy0H.jpg" + ')');
                     break;
                 case "scattered clouds":
                     $(".cloudy").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/CR9ArBs.jpg" + ')');
                     break;
                 case "broken clouds":
                     $(".cloudy").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/xrPyIQY.jpg" + ')');
                     break;
                 case "shower rain":
                     $(".sun-shower").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/ih6wEOr.jpg" + ')');
                     break;
                 case "rain":
                     $(".rainy").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/xpGU9uf.jpg" + ')');
                     break;
                 case "thunderstorm":
                     $(".thunder-storm").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/YTG9O3N.jpg" + ')');
                     break;
                 case "snow":
                     $(".flurries").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/qs7r20c.jpg" + ')');
                     break;
                 case "mist":
                     $(".cloudy").removeClass("hidden");
+                    $('body').css('background-image', 'url(' + "http://i.imgur.com/GaTPG7D.jpg" + ')');
                     break;
             }
         });
